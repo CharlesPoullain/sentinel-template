ebb0769404a72494c25f13ff94e0c4369f8db26a
Charles Poullain <charles.poullain85@gmail.com>
2024-06-23 21:45:56 +0200

    Feat: Add UnoCss config zith default preset

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), UnoCSS()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
