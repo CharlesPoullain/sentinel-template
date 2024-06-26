3ba28f6ce83865501e60acd0c7ff84e7b5404c8f
Charles <charles.poullain85@gmail.com>
2024-06-26 21:26:32 +0200

    Feat: Use FormItemWrapper as slotable component

---
+++ src/components/FormItemWrapper.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps({
+  title: String,
+  description: String
+})
+</script>
+
+<template>
+  <div class="grid gap-x-8 gap-y-6 sm:grid-cols-2 items-center border-t-zinc-800 b-t-solid py-10">
+    <div>
+      <h2 class="font-semibold text-lg m-0 p-0">{{ title }}</h2>
+      <span class="font-normal text-base text-zinc-500">{{ description }}</span>
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import BaseLayout from '@/components/BaseLayout.vue'
 import InputText from 'primevue/inputtext'
-
+import FormItemWrapper from '@/components/FormItemWrapper.vue'
 const formData = ref({
   teamName: 'Sentinel'
 })
@@ -13,19 +13,9 @@ const formData = ref({
     <div class="mx-10">
       <h1 class="text-white font-semibold text-2xl/8">Paramètres</h1>
 
-      <div
-        class="grid gap-x-8 gap-y-6 sm:grid-cols-2 items-center border-t-zinc-800 b-t-solid py-10"
-      >
-        <div>
-          <h2 class="font-semibold text-lg m-0 p-0">Nom de l'équipe</h2>
-          <span class="font-normal text-base text-zinc-500"
-            >Affiché sur votre profile publique.</span
-          >
-        </div>
-        <div>
-          <InputText type="text" v-model="formData.teamName" class="w-full" />
-        </div>
-      </div>
+      <FormItemWrapper title="Nom de l'équipe" description="Affiché sur votre profile publique.">
+        <InputText type="text" v-model="formData.teamName" class="w-full" />
+      </FormItemWrapper>
     </div>
   </BaseLayout>
 </template>
